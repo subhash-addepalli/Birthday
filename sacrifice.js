592a202ff0e86f6bdac1161f74b4b9a8d21fcb00
subhash-addepalli <subhashaddepalli@gmail.com>
2025-06-26 22:23:20 +0530

    Update sacrifice.js

--- sacrifice.js
+++ sacrifice.js
@@ -16,12 +16,7 @@ function sendMessage() {
   const phone = ["+91", "9948", "761", "423"].join("");
   const link = `https://wa.me/${phone}?text=${msg}`;
 
-  alert(
-    "Don't panic! WhatsApp Web will open in 10 seconds.\n" +
-    "Then tap the send icon."
-  );
-
   setTimeout(() => {
     window.open(link, '_blank'); // Opens WhatsApp in a new tab
-  }, 10000);
+  });
 }
